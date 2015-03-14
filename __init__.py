70cbcb19c6ca0d4b02dfae75d309e0b3440bed1d
pikpok <pikpok@riseup.net>
2015-03-14 12:27:57 +0100

    fixes, fixes, fixes  - added description parsing  - added pyhon shebangs  - changed encoding to helion's iso-8859-2  - changed indent to 4 spaces  - version bump to 0.0.4

--- __init__.py
+++ __init__.py
@@ -1,123 +1,124 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from __future__ import (unicode_literals, division, absolute_import,
                         print_function)
+
 import time
 
 from urllib import quote
+from lxml.html import fromstring, tostring
 
 from calibre.ebooks.metadata.sources.base import Source
 from calibre import browser, url_slash_cleaner
-from lxml.html import fromstring, tostring
 from calibre.utils.cleantext import clean_ascii_chars
 
 class Helion(Source):
 
-	name = 'Helion'
-	description	= _('Pobiera metadane z helion.pl')
-	author = 'pikpok'
-	supported_platforms = ['windows', 'osx', 'linux']
-	version = (0, 0, 3)
-	minimum_calibre_version = (0, 8, 0)
+    name = 'Helion'
+    description = _('Pobiera metadane z helion.pl')
+    author = 'pikpok'
+    supported_platforms = ['windows', 'osx', 'linux']
+    version = (0, 0, 4)
+    minimum_calibre_version = (0, 8, 0)
 
-	capabilities = frozenset(['identify', 'cover'])
-	touched_fields = frozenset(['title', 'authors', 'identifier:helion',
-		'identifier:isbn', 'rating', 'publisher', 'pubdate', 'languages'])
-	supports_gzip_transfer_encoding = True
+    capabilities = frozenset(['identify', 'cover'])
+    touched_fields = frozenset(['title', 'authors', 'identifier:helion',
+        'identifier:isbn', 'rating', 'publisher', 'pubdate', 'languages'])
+    supports_gzip_transfer_encoding = True
 
-	def identify(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
-		matches = []
-		br = self.browser
-		q = ''
-		title = title.replace('?','')
-		title = title.replace('ó', 'o')
-		title_tokens = list(self.get_title_tokens(title, strip_joiners=False, strip_subtitle=True))
-		if title_tokens:
-			tokens = [quote(t.encode('utf-8') if isinstance(t, unicode) else t) for t in title_tokens]
-			q = '+'.join(tokens)
-		if authors:
-			authors_tokens = self.get_author_tokens(authors, only_first_author=True)
-			if authors_tokens:
-				q += '+'
-				tokens = [quote(t.encode('utf-8') if isinstance(t, unicode) else t) for t in authors_tokens]
-			q += '+'.join(tokens)
-		
-		query = 'http://helion.pl/search?qa=&szukaj=%s&sortby=wd&wsprzed=1&wprzyg=1&wyczerp=1&sent=1'%(q)
+    def identify(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
+        matches = []
+        br = self.browser
+        q = ''
 
-		response = br.open_novisit(query, timeout=timeout)
-		raw = response.read().strip()
+        title_tokens = list(self.get_title_tokens(title, strip_joiners=False, strip_subtitle=True))
+        if title_tokens:
+            tokens = [quote(t.encode('iso-8859-2')) for t in title_tokens]
+            q += '+'.join(tokens)
 
-		root = fromstring(clean_ascii_chars(raw))
-		results = root.xpath('*//div[contains(@class,"search-helion")]')
-		log.info(results)
+        if authors:
+            authors_tokens = self.get_author_tokens(authors, only_first_author=True)
+            if authors_tokens:
+                q += '+'
+                tokens = [quote(t.encode('iso-8859-2')) for t in authors_tokens]
+            q += '+'.join(tokens)
 
-		for result in results:
-			book_url = result.xpath('./a[contains(@href,"ksiazki")]/@href')
-			matches.append(book_url)
+        query = 'http://helion.pl/search?qa=&szukaj=%s&sortby=wd&wsprzed=1&wprzyg=1&wyczerp=1&sent=1'%(q)
 
-		from calibre_plugins.helion.worker import Worker
-		workers = [Worker(url, result_queue, br, log, i, self) for i, url in enumerate(matches) if url]
-		for w in workers:
-			w.start()
-			time.sleep(0.1)
-		while not abort.is_set():
-			a_worker_is_alive = False
-			for w in workers:
-				w.join(0.2)
-				if abort.is_set():
-					break
-				if w.is_alive():
-					a_worker_is_alive = True
-			if not a_worker_is_alive:
-				break
-		return None
+        response = br.open_novisit(query, timeout=timeout)
+        raw = response.read().strip()
 
-	def download_cover(self, log, result_queue, abort, title = None, authors = None, identifiers = {}, timeout = 30, get_best_cover = False):
-		url = self.get_cached_cover_url(identifiers = identifiers)
-		br = self.browser
-		try:
-			cdata = br.open_novisit(url, timeout=timeout).read()
-			result_queue.put((self, cdata))
-		except:
-			log.exception('Failed to download cover from:', url)
+        root = fromstring(clean_ascii_chars(raw))
+        results = root.xpath('*//div[contains(@class,"search-helion")]')
 
-	def get_cached_cover_url(self, identifiers):
-		url = None
-		helion_id = identifiers.get('helion')
-		if helion_id is not None:
-			url = 'http://helion.pl/okladki/326x466/%s.jpg'%(helion_id)
-		return url
+        for result in results:
+            book_url = result.xpath('./a[contains(@href,"ksiazki")]/@href')
+            matches.append(book_url)
+
+        from calibre_plugins.helion.worker import Worker
+        workers = [Worker(url, result_queue, br, log, i, self) for i, url in enumerate(matches) if url]
+        for w in workers:
+            w.start()
+            time.sleep(0.1)
+        while not abort.is_set():
+            a_worker_is_alive = False
+            for w in workers:
+                w.join(0.2)
+                if abort.is_set():
+                    break
+                if w.is_alive():
+                    a_worker_is_alive = True
+            if not a_worker_is_alive:
+                break
+        return None
+
+    def download_cover(self, log, result_queue, abort, title = None, authors = None, identifiers = {}, timeout = 30, get_best_cover = False):
+        url = self.get_cached_cover_url(identifiers = identifiers)
+        br = self.browser
+        try:
+            cdata = br.open_novisit(url, timeout=timeout).read()
+            result_queue.put((self, cdata))
+        except:
+            log.exception('Failed to download cover from:', url)
+
+    def get_cached_cover_url(self, identifiers):
+        url = None
+        helion_id = identifiers.get('helion')
+        if helion_id is not None:
+            url = 'http://helion.pl/okladki/326x466/%s.jpg'%(helion_id)
+        return url
 
 if __name__ == '__main__':
-	'''
-	Tests
-	'''
-	from calibre.ebooks.metadata.sources.test import (test_identify_plugin, title_test, authors_test)
-	test_identify_plugin(Helion.name,
-		[
-		(
-			{
-			'title':'Ruby on Rails. Wprowadzenie',
-			'authors':['Bruce A. Tate & Curt Hibbs']
-			},
-			[
-			title_test('Ruby on Rails. Wprowadzenie'),
-			authors_test(['Bruce A. Tate', 'Curt Hibbs'])
-			]
-		)
-		]
-	)
-	test_identify_plugin(Helion.name,
-		[
-		(
-			{
-			'title':'jak pozostać anonimowym w sieci',
-			'authors':['sokół']
-			},
-			[
-			title_test('Jak pozostać anonimowym w sieci'),
-			authors_test(['Radosław Sokół'])
-			]
-		)
-		]
-	)
+    '''
+    Tests
+    '''
+    from calibre.ebooks.metadata.sources.test import (test_identify_plugin, title_test, authors_test)
+    test_identify_plugin(Helion.name,
+        [
+        (
+            {
+            'title':'Ruby on Rails. Wprowadzenie',
+            'authors':['Bruce A. Tate & Curt Hibbs']
+            },
+            [
+            title_test('Ruby on Rails. Wprowadzenie'),
+            authors_test(['Bruce A. Tate', 'Curt Hibbs'])
+            ]
+        )
+        ]
+    )
+    test_identify_plugin(Helion.name,
+        [
+        (
+            {
+            'title':u'Jak pozostać anonimowym w sieci',
+            'authors':[u'Radosław Sokół']
+            },
+            [
+            title_test(u'Jak pozostać anonimowym w sieci'),
+            authors_test([u'Radosław Sokół'])
+            ]
+        )
+        ]
+    )
--- worker.py
+++ worker.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from threading import Thread
 
 from lxml.html import fromstring, tostring
@@ -10,75 +13,79 @@ from calibre.ebooks.metadata.sources.base import fixcase, fixauthors, cap_author
 
 class Worker(Thread):
 
-	def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=20):
-		Thread.__init__(self)
-		self.daemon = True
-		self.url, self.result_queue = url[0], result_queue
-		self.log, self.timeout = log, timeout
-		self.relevance, self.plugin = relevance, plugin
-		self.browser = browser.clone_browser()
-		self.cover_url = self.helion_id = self.isbn = None
+    def __init__(self, url, result_queue, browser, log, relevance, plugin, timeout=20):
+        Thread.__init__(self)
+        self.daemon = True
+        self.url, self.result_queue = url[0], result_queue
+        self.log, self.timeout = log, timeout
+        self.relevance, self.plugin = relevance, plugin
+        self.browser = browser.clone_browser()
+        self.cover_url = self.helion_id = self.isbn = None
 
-	def run(self):
-		raw = self.browser.open_novisit(self.url, timeout=self.timeout).read().strip()
-		root = fromstring(clean_ascii_chars(raw))
+    def run(self):
+        raw = self.browser.open_novisit(self.url, timeout=self.timeout).read().strip()
+        root = fromstring(clean_ascii_chars(raw))
 
-		helion_id = re.search('ksiazki/(.*).htm', self.url).groups(0)[0]
+        helion_id = re.search('ksiazki/(.*).htm', self.url).groups(0)[0]
 
-		title = unicode(root.xpath('//div[@class="book_title"]/h1/span/text()')[0])
+        title = unicode(root.xpath('//div[@class="book_title"]/h1/span/text()')[0])
 
-		author_node = root.xpath('///div[@class="book_title"]/p/descendant::text()')
-		authors = []
-		for i in range(1, len(author_node), 2):
-			authors.append(unicode(author_node[i]))
-		print(authors)
+        author_node = root.xpath('///div[@class="book_title"]/p/descendant::text()')
+        authors = []
+        for i in range(1, len(author_node), 2):
+            authors.append(unicode(author_node[i]))
+        print(authors)
 
-		mi = Metadata(title,authors)
-		
-		mi.set_identifier('helion', helion_id)
-		self.helion_id = helion_id
+        mi = Metadata(title,authors)
+        
+        mi.set_identifier('helion', helion_id)
+        self.helion_id = helion_id
 
-		#TODO: parse series
+        #TODO: parse series
 
-		isbn = re.search(', (\d{13})$', root.xpath('//li[@itemprop="isbn"]/text()')[0])
-		if not isbn:
-			isbn = re.search(', (\d{10})$', root.xpath('//li[@itemprop="isbn"]/text()')[0])
-		mi.isbn = self.isbn = isbn.groups(0)[0]
+        isbn = re.search(', (\d{13})$', root.xpath('//li[@itemprop="isbn"]/text()')[0])
+        if not isbn:
+            isbn = re.search(', (\d{10})$', root.xpath('//li[@itemprop="isbn"]/text()')[0])
+        mi.isbn = self.isbn = isbn.groups(0)[0]
 
-		mi.rating = 5*float(root.xpath('//span[@itemprop="ratingValue"]/text()')[0])/6
+        mi.rating = 5*float(root.xpath('//span[@itemprop="ratingValue"]/text()')[0])/6
 
-		#TODO: parse comments
-		#TODO: parse tags
-		
-		self.cover_url = self.parse_cover(helion_id)
-		mi.has_cover = bool(self.cover_url)
+        x = root.xpath('//div[@class="center-body-center"]/*[not(@class="linkis")]')
+        mi.comments = ''
+        for tag in x:
+            mi.comments += tostring(tag)
 
-		mi.publisher = 'Helion'
+        #TODO: parse tags
 
-		date = root.xpath('//div[@id="center-body-szczegoly"]/ul/li[contains(text(),"drukowanej")]/text()')[1]
-		year = int(re.search(': (\d{4})-', date).groups(0)[0])
-		month = int(re.search('-(\d{2})-', date).groups(0)[0])
-		day = int(re.search('-(\d{2})$', date).groups(0)[0])
-		mi.pubdate = datetime.datetime(year, month, day)
+        self.cover_url = self.parse_cover(helion_id)
+        mi.has_cover = bool(self.cover_url)
 
-		mi.languages = ["pol"]
+        mi.publisher = 'Helion'
 
-		self.plugin.cache_isbn_to_identifier(self.isbn, self.helion_id)
-		self.plugin.cache_identifier_to_cover_url(self.helion_id, self.cover_url)
+        date = root.xpath('//div[@id="center-body-szczegoly"]/ul/li[contains(text(),"drukowanej")]/text()')[1]
+        year = int(re.search(': (\d{4})-', date).groups(0)[0])
+        month = int(re.search('-(\d{2})-', date).groups(0)[0])
+        day = int(re.search('-(\d{2})$', date).groups(0)[0])
+        mi.pubdate = datetime.datetime(year, month, day)
 
-		mi.source_relevance = self.relevance
-		self.clean_downloaded_metadata(mi)
-		self.result_queue.put(mi)
+        mi.languages = ["pol"]
 
-	def parse_cover(self, helion_id):
-		url = 'http://helion.pl/okladki/326x466/%s.jpg'%self.helion_id
-		info = self.browser.open_novisit(url, timeout=self.timeout).info()
-		if int(info.getheader('Content-Length')) > 1000:
-			return url
-		else:
-			self.log.warning('Nie ma okladki: %s'%url)
+        self.plugin.cache_isbn_to_identifier(self.isbn, self.helion_id)
+        self.plugin.cache_identifier_to_cover_url(self.helion_id, self.cover_url)
 
-	def clean_downloaded_metadata(self, mi):
-		mi.authors = fixauthors(mi.authors)
-		mi.tags = list(map(fixcase, mi.tags))
-	 	mi.isbn = check_isbn(mi.isbn)
+        mi.source_relevance = self.relevance
+        self.clean_downloaded_metadata(mi)
+        self.result_queue.put(mi)
+
+    def parse_cover(self, helion_id):
+        url = 'http://helion.pl/okladki/326x466/%s.jpg'%self.helion_id
+        info = self.browser.open_novisit(url, timeout=self.timeout).info()
+        if int(info.getheader('Content-Length')) > 1000:
+            return url
+        else:
+            self.log.warning('Nie ma okladki: %s'%url)
+
+    def clean_downloaded_metadata(self, mi):
+        mi.authors = fixauthors(mi.authors)
+        mi.tags = list(map(fixcase, mi.tags))
+        mi.isbn = check_isbn(mi.isbn)
